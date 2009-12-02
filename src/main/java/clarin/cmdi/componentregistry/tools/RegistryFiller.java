ef5b59329163897417d76e95bbf5632bd7cf0a5b
Patrick Duin <Patrick.Duin@mpi.nl>
2009-12-02 08:41:05 +0000

    * Made initial command line tool to fill the registry. Not very robust yet but workable. Meant for internal use only. Read the pom.xml comment for building instructions

---
+++ src/main/java/clarin/cmdi/componentregistry/tools/RegistryFiller.java
@@ -0,0 +1,80 @@
+package clarin.cmdi.componentregistry.tools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import clarin.cmdi.componentregistry.ComponentRegistryImpl;
+import clarin.cmdi.componentregistry.Configuration;
+import clarin.cmdi.componentregistry.model.RegisterResponse;
+import clarin.cmdi.componentregistry.rest.ComponentRegistryRestService;
+
+public class RegistryFiller {
+
+    private final static Logger LOG = LoggerFactory.getLogger(Configuration.class);
+
+    private static final String REGISTRY_ROOT = "/Users/patdui/Workspace/Clarin/MyRegistry"; //"/tmp/componentRegistryTest";
+    private ComponentRegistryRestService service;
+
+    RegistryFiller() {
+        ComponentRegistryImpl registry = (ComponentRegistryImpl) ComponentRegistryImpl.getInstance();
+        Configuration configuration = new Configuration();
+        configuration.setRegistryRoot(new File(REGISTRY_ROOT));
+        configuration.init();
+        registry.setConfiguration(configuration);
+        service = new ComponentRegistryRestService();
+    }
+
+    /**
+     * RegistryFiller "P.Duin" "Test files" imdi -c /Users/patdui/Workspace/Clarin/metadata/toolkit/components/imdi/component*.xml
+     * @param args
+     */
+    public static void main(String[] args) {
+        LOG.info("RegistryFiller started with arguments: " + Arrays.toString(args));
+        RegistryFiller filler = new RegistryFiller();
+        String creatorName = args[0];
+        String description = args[1];
+        String group = args[2];
+        boolean registerProfiles = "-p".equals(args[3]); //Otherwise -c
+        for (int i = 4; i < args.length; i++) {
+            File file = new File(args[i]);
+            LOG.info("Registering " + (i - 3) + "/" +(args.length - 4) + ": "+file.getName());
+            try {
+                if (registerProfiles) {
+                    filler.registerProfile(file, creatorName, description);
+                } else {
+                    filler.registerComponent(file, creatorName, description, group);
+                }
+            } catch (FileNotFoundException e) {
+                LOG.error("Error:", e);
+            }
+        }
+    }
+
+    private void registerProfile(File file, String creatorName, String description) throws FileNotFoundException {
+        RegisterResponse registeredProfile = service.registeredProfile(new FileInputStream(file), file.getName(), creatorName, description);
+        handleResult(registeredProfile);
+    }
+
+    private void registerComponent(File file, String creatorName, String description, String group) throws FileNotFoundException {
+        RegisterResponse registeredComponent = service.registeredComponent(new FileInputStream(file), file.getName(), creatorName,
+                description, group);
+        handleResult(registeredComponent);
+    }
+
+    private void handleResult(RegisterResponse response) {
+        if (response.isRegistered()) {
+            LOG.info("Registration ok.");
+        } else {
+            LOG.error("Registration failed with the following errors:");
+            for (int i = 0; i < response.getErrors().size(); i++) {
+                LOG.error(response.getErrors().get(i));
+            }
+        }
+    }
+
+}
