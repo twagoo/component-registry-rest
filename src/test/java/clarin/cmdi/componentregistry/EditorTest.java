ecdb49f8b22d41a47d8f0f14042e0c35532c16fe
Patrick Duin <Patrick.Duin@mpi.nl>
2009-12-11 13:47:20 +0000

    * disabled my fake test

--- src/test/java/clarin/cmdi/componentregistry/EditorTest.java
+++ src/test/java/clarin/cmdi/componentregistry/EditorTest.java
@@ -14,14 +14,14 @@ public class EditorTest {
 
     @Test
     public void testCreateProfile() throws FileNotFoundException, JAXBException, UnsupportedEncodingException {
-        CMDComponentSpec locComponent = MDMarshaller.unmarshal(CMDComponentSpec.class, new File(
-                "/Users/patdui/Workspace/Clarin/metadata/toolkit/components/imdi/component-location.xml"));
-        CMDComponentSpec writtenResourceComponent = MDMarshaller.unmarshal(CMDComponentSpec.class, new File(
-                "/Users/patdui/Workspace/Clarin/metadata/toolkit/components/imdi/component-keys.xml"));
+//        CMDComponentSpec locComponent = MDMarshaller.unmarshal(CMDComponentSpec.class, new File(
+//                "/Users/patdui/Workspace/Clarin/metadata/toolkit/components/imdi/component-location.xml"));
+//        CMDComponentSpec writtenResourceComponent = MDMarshaller.unmarshal(CMDComponentSpec.class, new File(
+//                "/Users/patdui/Workspace/Clarin/metadata/toolkit/components/imdi/component-keys.xml"));
 
-        Editor editor = new Editor();
-        String result = editor.createProfile(locComponent.getCMDComponent().get(0), writtenResourceComponent.getCMDComponent().get(0));
-        System.out.println(result);
+//        Editor editor = new Editor();
+//        String result = editor.createProfile(locComponent.getCMDComponent().get(0), writtenResourceComponent.getCMDComponent().get(0));
+//        System.out.println(result);
 
         //TODO Make real tests
     }
