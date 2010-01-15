5f9f06409202f9550764dc78742bd1555e62bbff
Patrick Duin <Patrick.Duin@mpi.nl>
2010-01-15 14:56:53 +0000

    * Refactored RegistryFiller and added RegistryMigration a tool to migrate a registry dir into an regisrty url

--- src/main/java/clarin/cmdi/componentregistry/tools/RegistryFiller.java
+++ src/main/java/clarin/cmdi/componentregistry/tools/RegistryFiller.java
@@ -1,20 +1,23 @@
 package clarin.cmdi.componentregistry.tools;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 import javax.xml.bind.JAXBException;
 
@@ -27,166 +30,83 @@ import clarin.cmdi.componentregistry.MDMarshaller;
 import clarin.cmdi.componentregistry.components.CMDComponentSpec;
 import clarin.cmdi.componentregistry.components.CMDComponentType;
 import clarin.cmdi.componentregistry.model.ComponentDescription;
-import clarin.cmdi.componentregistry.model.RegisterResponse;
-import clarin.cmdi.componentregistry.rest.ComponentRegistryRestService;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.multipart.FormDataMultiPart;
-
 
 /**
- * Disclaimer:
- * This class is only usable to automate the initialisation of an ComponentRegistry. 
- * It fill the registry with components/profiles from the file system, it is not meant to be used as a quick help setting up a Clarin Registry. 
- * Use the registry web interface for "proper" adding of profiles/components.
- *
+ * Disclaimer: This class is only usable to automate the initialisation of an ComponentRegistry. It fill the registry with
+ * components/profiles from the file system, it is not meant to be used as a quick help setting up a Clarin Registry. Use the registry web
+ * interface for "proper" adding of profiles/components.
+ * 
  */
 public class RegistryFiller {
 
     private final static Logger LOG = LoggerFactory.getLogger(RegistryFiller.class);
 
-    private static final File WORK_DIR = new File("/tmp/RegistryFiller");
-
     private WebResource service;
 
-    private static int failed = 0;
+    private int failed = 0;
 
-    private Set<File> unresolvedFiles;
+    private Set<RegObject> unresolvedComponents;
+    private Map<RegObject, CMDComponentSpec> resolvedComponents;
 
-    private List<File> resolvedFiles;
+    private List<RegObject> profiles = new ArrayList<RegObject>();
+    private List<RegObject> components = new ArrayList<RegObject>();
 
-    RegistryFiller() {
-        WORK_DIR.mkdir();
-        LOG.info("Will write resolved components in "+WORK_DIR);
-        URI uri = UriBuilder.fromUri("http://localhost/").port(8080).path("ComponentRegistry/rest/registry").build();
+    private RegistryToolHelper helper;
+
+    public RegistryFiller(String url) {
+        URI uri = UriBuilder.fromUri(url).build();
         Client client = Client.create();
         service = client.resource(uri);
-        unresolvedFiles = new HashSet<File>();
-        resolvedFiles = new ArrayList<File>();
+        unresolvedComponents = new HashSet<RegObject>();
+        resolvedComponents = new HashMap<RegObject, CMDComponentSpec>();
+        helper = new RegistryToolHelper(service);
     }
 
     /**
      * RegistryFiller "P.Duin" "Test files" imdi -c /Users/patdui/Workspace/Clarin/metadata/toolkit/components/imdi/component*.xml
      * @param args
      * 
-     * Uses a heuristic to resolve components which are linked together through fileName. 
-     * It will try to find the component with a name equal to the filename (without extension) and set the registered id correct.
+     *            Uses a heuristic to resolve components which are linked together through fileName. It will try to find the component with
+     *            a name equal to the filename (without extension) and set the registered id correct.
      * 
      */
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         LOG.info("RegistryFiller started with arguments: " + Arrays.toString(args));
         if (args.length == 0 || args.length < 5) {
             printUsage();
         }
-
-        RegistryFiller filler = new RegistryFiller();
+        String url = "http://localhost:8080/ComponentRegistry/rest/registry";
+        RegistryFiller filler = new RegistryFiller(url);
         String creatorName = args[0];
         String description = args[1];
         String group = args[2];
         boolean registerProfiles = "-p".equals(args[3]); //Otherwise -c
+        int nrOfFailed = 0;
         for (int i = 4; i < args.length; i++) {
             File file = new File(args[i]);
-            LOG.info("Registering " + (i - 3) + "/" + (args.length - 4) + ": " + file.getName());
+            LOG.info("Adding " + (i - 3) + "/" + (args.length - 4) + ": " + file.getName());
             try {
                 if (registerProfiles) {
-                    filler.registerProfile(file, creatorName, description);
+                    filler.addProfile(file, FilenameUtils.getBaseName(file.getName()), creatorName, description);
                 } else {
-                    filler.replaceFileNameForIds(file);
+                    filler.addComponent(file, FilenameUtils.getBaseName(file.getName()), creatorName, description, group);
                 }
             } catch (Exception e) {
-                failed++;
+                nrOfFailed++;
                 LOG.error("Error in file: " + file, e);
             }
         }
-        try {
-            filler.registerComponents(creatorName, description, group);
-        } catch (Exception e) {
-            failed++;
-            LOG.error("Error:", e);
-        }
-        if (failed > 0) {
-            LOG.error("Failed to register " + failed + " components/profiles.");
+        nrOfFailed = filler.register();
+        if (nrOfFailed > 0) {
+            LOG.error("Failed to register " + nrOfFailed + " components/profiles.");
         } else {
             LOG.info("Everything registered ok.");
         }
-    }
-
-    private void replaceFileNameForIds(File file) throws JAXBException, IOException {
-        CMDComponentSpec comp = MDMarshaller.unmarshal(CMDComponentSpec.class, file);
-        if (comp == null) {
-            failed++;
-            return;
-        }
-        List<CMDComponentType> cmdComponents = comp.getCMDComponent();
-        boolean resolved = replaceFileNames(cmdComponents);
-        if (!resolved) {
-            unresolvedFiles.add(file);
-        } else {
-            File newFile = new File(WORK_DIR, file.getName());
-            if (newFile.exists()) {
-                newFile.delete();
-            }
-            newFile.createNewFile();
-            MDMarshaller.marshal(comp, new FileOutputStream(newFile));
-            resolvedFiles.add(newFile);
-        }
-    }
-
-    private boolean replaceFileNames(List<CMDComponentType> cmdComponents) {
-        boolean resolved = true;
-        for (CMDComponentType cmdComponentType : cmdComponents) {
-            if (cmdComponentType.getComponentId() == null && cmdComponentType.getName() == null) { //otherwise inline or top level component no need to do anything
-                String name = FilenameUtils.getBaseName(cmdComponentType.getFilename());
-                    List<ComponentDescription> descriptions = service.path("/components").get(new GenericType<List<ComponentDescription>>() {
-                    });
-                    for (ComponentDescription componentDescription : descriptions) {
-                        if (componentDescription.getName().equals(name)) {
-                            LOG.info("Replacing fileName: " + cmdComponentType.getFilename() + " with componentId: "
-                                    + componentDescription.getId());
-                            cmdComponentType.setComponentId(componentDescription.getId());
-                            break;
-                        }
-                    }
-                if (cmdComponentType.getComponentId() == null) {
-                    resolved = false;
-                }
-            }
-            if (resolved) {
-                resolved = replaceFileNames(cmdComponentType.getCMDComponent());
-            }
-        }
-        return resolved;
-    }
-
-    private void registerComponents(String creatorName, String description, String group) throws IOException, JAXBException {
-        for (File file : resolvedFiles) {
-            registerComponent(file, creatorName, description, group);
-        }
-        resolvedFiles = new ArrayList<File>();
-        Set<File> unresolvedCopy = new HashSet<File>(unresolvedFiles);
-        unresolvedFiles = new HashSet<File>();
-        for (File file : unresolvedCopy) {
-            replaceFileNameForIds(file);
-        }
-        if (unresolvedFiles.size() < unresolvedCopy.size()) {
-            registerComponents(creatorName, description, group);// recursion
-        } else if (!unresolvedFiles.isEmpty()){
-            LOG.error("Cannot resolve nested components manual intervention is needed.");
-            LOG.error("Files that could not be registered are:");
-            for (File file : unresolvedFiles) {
-                LOG.error("- "+file);
-            }
-            failed += unresolvedFiles.size();
-        }
-    }
-
-    private void registerComponent(File file, String creatorName, String description, String group) throws IOException {
-        FormDataMultiPart form = createForm(file, creatorName, description);
-        form.field(ComponentRegistryRestService.GROUP_FORM_FIELD, group);
-        RegisterResponse response = service.path("/components").type(MediaType.MULTIPART_FORM_DATA).post(RegisterResponse.class, form);
-        handleResult(response);
     }
 
     private static void printUsage() {
@@ -194,32 +114,191 @@ public class RegistryFiller {
         System.exit(0);
     }
 
-    private void registerProfile(File file, String creatorName, String description) throws IOException {
-        FormDataMultiPart form = createForm(file, creatorName, description);
-        RegisterResponse response = service.path("/profiles").type(MediaType.MULTIPART_FORM_DATA).post(RegisterResponse.class, form);
-        handleResult(response);
+    public void addProfile(File file, String name, String creatorName, String description) {
+        profiles.add(new RegObject(file, name, creatorName, description, null));
     }
 
-    private FormDataMultiPart createForm(File file, String creatorName, String description) throws IOException {
-        FormDataMultiPart form = new FormDataMultiPart();
-        Reader reader = new InputStreamReader(new FileInputStream(file), "UTF-8"); //To handle unicode chars.
-        form.field(ComponentRegistryRestService.DATA_FORM_FIELD, new ByteArrayInputStream(IOUtils.toByteArray(reader, "UTF-8")),
-                MediaType.APPLICATION_OCTET_STREAM_TYPE);
-        form.field(ComponentRegistryRestService.NAME_FORM_FIELD, FilenameUtils.getBaseName(file.getName()));
-        form.field(ComponentRegistryRestService.DESCRIPTION_FORM_FIELD, description);
-        form.field(ComponentRegistryRestService.CREATOR_NAME_FORM_FIELD, creatorName);
-        return form;
+    public void addComponent(File file, String name, String creatorName, String description, String group) {
+        components.add(new RegObject(file, name, creatorName, description, group));
     }
 
-    private void handleResult(RegisterResponse response) {
-        if (response.isRegistered()) {
-            LOG.info("Registration ok.");
-        } else {
-            failed++;
-            LOG.error("Registration failed with the following errors:");
-            for (int i = 0; i < response.getErrors().size(); i++) {
-                LOG.error(response.getErrors().get(i));
+    public int register() {
+        LOG.info("Registering " + components.size() + " components ...");
+        register(components, false);
+        LOG.info("Registering " + profiles.size() + " profiles ...");
+        register(profiles, true);
+        failed += helper.getNrOfFailed();
+        return failed;
+    }
+
+    private void register(List<RegObject> regObjects, boolean registerProfiles) {
+        for (int i = 0; i < regObjects.size(); i++) {
+            RegObject regObject = regObjects.get(i);
+            LOG.info("Resolving " + (i + 1) + "/" + regObjects.size() + ": " + regObject.getName());
+            try {
+                if (registerProfiles) {
+                    registerProfile(regObject);
+                } else {
+                    replaceFileNameForIds(regObject);
+                }
+            } catch (Exception e) {
+                failed++;
+                LOG.error("Error in file: " + regObject.getFile(), e);
             }
+        }
+        if (!registerProfiles) { //extra step for components
+            try {
+                registerComponents();
+            } catch (Exception e) {
+                failed++;
+                LOG.error("Error:", e);
+            }
+        }
+    }
+
+    private void registerProfile(RegObject regObject) throws IOException {
+        helper.registerProfile(createInputStream(regObject.getFile()), regObject.getCreatorName(), regObject.getDescription(), regObject
+                .getName());
+    }
+
+    private void replaceFileNameForIds(RegObject regObject) throws JAXBException, IOException {
+        CMDComponentSpec comp = MDMarshaller.unmarshal(CMDComponentSpec.class, regObject.getFile());
+        if (comp == null) {
+            failed++;
+            return;
+        }
+        List<CMDComponentType> cmdComponents = comp.getCMDComponent();
+        boolean resolved = replaceFileNames(cmdComponents);
+        if (!resolved) {
+            unresolvedComponents.add(regObject);
+        } else {
+            resolvedComponents.put(regObject, comp);
+        }
+    }
+
+    private boolean replaceFileNames(List<CMDComponentType> cmdComponents) {
+        boolean resolved = true;
+        for (CMDComponentType cmdComponentType : cmdComponents) {
+            if (cmdComponentType.getFilename() != null) { //nested component so try to resolve the fileName to the id.
+                String name = FilenameUtils.getBaseName(cmdComponentType.getFilename());
+                List<ComponentDescription> descriptions = service.path("/components").get(new GenericType<List<ComponentDescription>>() {
+                });
+                for (ComponentDescription componentDescription : descriptions) {
+                    if (componentDescription.getName().equals(name)) { //NOTE: assuming here name is unique which is not guaranteed. Need to check manually if actually correct. Should be ok most of the times.
+                        LOG.info("Replacing fileName: " + cmdComponentType.getFilename() + " with componentId: "
+                                + componentDescription.getId());
+                        cmdComponentType.setComponentId(componentDescription.getId());
+                        break;
+                    }
+                }
+                resolved &= componentExist(cmdComponentType.getComponentId());
+            } else {
+                resolved &= replaceFileNames(cmdComponentType.getCMDComponent());//Recursion
+            }
+        }
+        return resolved;
+    }
+
+    private boolean componentExist(String id) {
+        try {
+            service.path("/components/" + id).get(CMDComponentSpec.class);
+        } catch (UniformInterfaceException e) {
+            if (e.getResponse().getStatus() != HttpURLConnection.HTTP_NO_CONTENT) {
+                LOG.error("Unexpected exception while getting component: ", e);
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private void registerComponents() throws IOException, JAXBException {
+        int i = 1;
+        for (RegObject regObject : resolvedComponents.keySet()) {
+            LOG.info("Registering " + i++ + "/" + resolvedComponents.size() + ": " + regObject.getName());
+            CMDComponentSpec comp = resolvedComponents.get(regObject);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            MDMarshaller.marshal(comp, out);
+            ByteArrayInputStream input = new ByteArrayInputStream(out.toByteArray());
+            helper.registerComponent(input, regObject.getCreatorName(), regObject.getDescription(), regObject.getGroup(), regObject
+                    .getName());
+        }
+        resolvedComponents = new HashMap<RegObject, CMDComponentSpec>();
+        Set<RegObject> unresolvedCopy = new HashSet<RegObject>(unresolvedComponents);
+        unresolvedComponents = new HashSet<RegObject>();
+        for (RegObject regObject : unresolvedCopy) {
+            replaceFileNameForIds(regObject);
+        }
+        if (unresolvedComponents.size() < unresolvedCopy.size()) {
+            registerComponents();// recursion
+        } else if (!unresolvedComponents.isEmpty()) {
+            LOG.error("Cannot resolve nested components manual intervention is needed.");
+            LOG.error("Files that could not be registered are:");
+            for (RegObject regObject : unresolvedComponents) {
+                LOG.error("- " + regObject.getFile());
+            }
+            failed += unresolvedComponents.size();
+        }
+    }
+
+    private InputStream createInputStream(File file) throws IOException {
+        Reader reader = new InputStreamReader(new FileInputStream(file), "UTF-8"); //To handle unicode chars.
+        InputStream input = new ByteArrayInputStream(IOUtils.toByteArray(reader, "UTF-8"));
+        return input;
+    }
+
+    private class RegObject {
+
+        private final File file;
+        private final String name;
+        private final String creatorName;
+        private final String description;
+        private final String group;
+
+        public RegObject(File file, String name, String creatorName, String description, String group) {
+            this.file = file;
+            this.name = name;
+            this.creatorName = creatorName;
+            this.description = description;
+            this.group = group;
+        }
+
+        public File getFile() {
+            return file;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getCreatorName() {
+            return creatorName;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getGroup() {
+            return group;
+        }
+
+        @Override
+        public int hashCode() {
+            return file.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof RegObject) {
+                RegObject other = (RegObject) obj;
+                return file.equals(other.getFile());
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "name= " + name + "\t file= " + file.toString();
         }
     }
 
---
+++ src/main/java/clarin/cmdi/componentregistry/tools/RegistryMigration.java
@@ -0,0 +1,85 @@
+package clarin.cmdi.componentregistry.tools;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import clarin.cmdi.componentregistry.ComponentRegistryImpl;
+import clarin.cmdi.componentregistry.Configuration;
+import clarin.cmdi.componentregistry.model.ComponentDescription;
+import clarin.cmdi.componentregistry.model.ProfileDescription;
+
+/**
+ * Can be used to migrate a registry from one location(Registry Dir) into another Registry by url.
+ * 
+ */
+public class RegistryMigration {
+
+    private final static Logger LOG = LoggerFactory.getLogger(RegistryMigration.class);
+    private Configuration config;
+    private final String targetUrl;
+    private ComponentRegistryImpl registry;
+
+    /*
+     * http://lux16.mpi.nl:8080/ds/ComponentRegistry/rest/registry
+     * 
+     * /tmp/ComponentRegistry
+     */
+    public RegistryMigration(String sourceDir, String targetUrl) {
+        this.targetUrl = targetUrl;
+        config = new Configuration();
+        config.setRegistryRoot(new File(sourceDir));
+        config.init();
+        registry = (ComponentRegistryImpl) ComponentRegistryImpl.getInstance();
+        registry.setConfiguration(config);
+    }
+
+    private void migrate() {
+        RegistryFiller filler = new RegistryFiller(targetUrl);
+        addComponents(filler);
+        addProfiles(filler);
+        int nrOfFailed = filler.register();
+        if (nrOfFailed > 0) {
+            LOG.error("Failed to migrate " + nrOfFailed + " components/profiles.");
+        } else {
+            LOG.info("Everything migrated ok.");
+        }
+    }
+
+    private void addComponents(RegistryFiller filler) {
+        List<ComponentDescription> descriptions = registry.getComponentDescriptions();
+        for (ComponentDescription desc : descriptions) {
+            File file = registry.getComponentFile(desc.getId());
+            filler.addComponent(file, desc.getName(), desc.getCreatorName(), desc.getDescription(), desc.getGroupName());
+        }
+    }
+
+    private void addProfiles(RegistryFiller filler) {
+        List<ProfileDescription> descriptions = registry.getProfileDescriptions();
+        for (ProfileDescription desc : descriptions) {
+            File file = registry.getComponentFile(desc.getId());
+            filler.addProfile(file, desc.getName(), desc.getCreatorName(), desc.getDescription());
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        LOG.info("RegistryMigration started with arguments: " + Arrays.toString(args));
+        if (args.length != 2) {
+            printUsage();
+        }
+        RegistryMigration migration = new RegistryMigration(args[0], args[1]);
+        migration.migrate();
+    }
+
+    private static void printUsage() {
+        System.out.println("usage: <source registry directory> <target registry URL>");
+        System.exit(0);
+    }
+
+}
