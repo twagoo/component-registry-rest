dfca06868cf72a2a4bdfa076880f54c1f05e6f3f
Patrick Duin <Patrick.Duin@mpi.nl>
2010-06-08 07:15:45 +0000

    * added type parameter and always search in profile metadata for isocatServlet

--- ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/servlet/IsocatServlet.java
+++ ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/servlet/IsocatServlet.java
@@ -9,18 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriBuilder;
 
 import clarin.cmdi.componentregistry.Configuration;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 /**
  * 
- * Servlet responsible for providing a bridge to the isocat REST interface.
- * Can be called by the frontend to circumvent crosscripting and FLASH/Browser limitation in setting the headers of a request. 
- *
+ * Servlet responsible for providing a bridge to the isocat REST interface. Can be called by the frontend to circumvent crosscripting and
+ * FLASH/Browser limitation in setting the headers of a request.
+ * 
  */
 public class IsocatServlet extends HttpServlet {
 
@@ -38,10 +40,18 @@ public class IsocatServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
+        queryParams.add("dcif-mode", "list");
         String keywords = req.getParameter("keywords");
+        queryParams.add("keywords", keywords);
+        String type = req.getParameter("type"); //simple, complex or no type param to search in everything
+        if (type != null) {
+            queryParams.add("type", type);
+        }
+        queryParams.add("profile", "Metadata"); //always search in metadata profile
         resp.setContentType(MediaType.TEXT_XML);
-        String result = service.path("/user/guest/search").queryParam("keywords", keywords).queryParam("dcif-mode", "list").accept(DCIF_XML).get(
-                String.class);
+        //&type=simple&profile=Metadata
+        String result = service.path("/user/guest/search").queryParams(queryParams).accept(DCIF_XML).get(String.class);
         resp.getWriter().write(result);
         resp.flushBuffer();
     }
