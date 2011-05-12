f4fc200531ef0f1f0287a29ef089a2a689bec26b
Twan Goosen <t.goosen@gmail.com>
2011-05-12 12:01:05 +0000

    Added test method for injection. This also causes test class not to fail because of no runnable test

--- ComponentRegistry/src/test/java/clarin/cmdi/componentregistry/impl/database/ComponentRegistryFactoryDbImplTest.java
+++ ComponentRegistry/src/test/java/clarin/cmdi/componentregistry/impl/database/ComponentRegistryFactoryDbImplTest.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertNotNull;
+
 /**
  *
  * @author Twan Goosen <twan.goosen@mpi.nl>
@@ -17,4 +19,9 @@ public class ComponentRegistryFactoryDbImplTest {
 
     @Autowired
     ComponentRegistryFactoryDbImpl componentRegistryFactory;
+
+    @Test
+    public void testInjection(){
+        assertNotNull(componentRegistryFactory);
+    }
 }
