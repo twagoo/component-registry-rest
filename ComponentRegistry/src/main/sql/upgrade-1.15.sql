d4bf825e5047ea9d01a973c5a8edbe95d166b2ce
George.Georgovassilis <George.Georgovassilis@mpi.nl>
2013-10-15 15:36:04 +0000

    #409: Fixed database upgrade script

--- ComponentRegistry/src/main/sql/upgrade-1.15.sql
+++ ComponentRegistry/src/main/sql/upgrade-1.15.sql
@@ -1,12 +1,18 @@
-ALTER TABLE profile_description RENAME TO persistentcomponent;
+ALTER TABLE profile_description RENAME TO persistentcomponents;
 
-alter table persistentcomponent
-rename column profile_id to component_id;
+alter table persistentcomponents
+ rename column profile_id to component_id;
 
-insert into persistentcomponent
-select * from component_description;
+insert into persistentcomponents(user_id, content_id, is_public, is_deleted, component_id, name, description, registration_date, 
+ creator_name, domain_name, group_name, href, show_in_editor)
+ select user_id, content_id,is_public, is_deleted, component_id, name, description, registration_date, 
+ creator_name, domain_name,  group_name, href, true
+ from component_description;
+
+alter table comments drop constraint comments_component_id;
 
 update comments set component_description_id = profile_description_id where component_description_id is null;
+
 alter table comments rename column component_description_id to component_id;
 alter table comments drop profile_description_id;
 
