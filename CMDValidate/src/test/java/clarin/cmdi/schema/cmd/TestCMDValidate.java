e614cc21af7f99844a3a5fffa15781efde79e5d0
twagoo <t.goosen@gmail.com>
2014-03-13 10:41:44 +0000

    merged changes in CMDValdite from trunk (now versioned as 1.2)

--- CMDValidate/src/test/java/clarin/cmdi/schema/cmd/TestCMDValidate.java
+++ CMDValidate/src/test/java/clarin/cmdi/schema/cmd/TestCMDValidate.java
@@ -21,140 +21,165 @@ public class TestCMDValidate {
 
     @Before
     public void setUp() {
-	cmdValidator = new Validator(getClass().getResource("/schema/general-component-schema.xsd"));
-	cmdValidator.setResourceResolver(new ResourceResolver());
+        cmdValidator = new Validator(getClass().getResource("/schema/general-component-schema.xsd"));
+        cmdValidator.setResourceResolver(new ResourceResolver());
     }
 
     @After
     public void tearDown() {
-	cmdValidator = null;
+        cmdValidator = null;
     }
 
     protected boolean validate(String prof) throws Exception {
-	System.out.print("Test CMD validation [" + prof + "] ");
-	Source src = new javax.xml.transform.stream.StreamSource(new java.io.File(TestCMDValidate.class.getResource("/docs/" + prof).toURI()));
-	boolean valid = cmdValidator.validateProfile(src);
-	if (valid) {
-	    System.out.println("valid");
-	} else {
-	    System.out.println("invalid");
-	}
-	cmdValidator.printMessages(System.out);
-	return valid;
+        System.out.print("Test CMD validation [" + prof + "] ");
+        Source src = new javax.xml.transform.stream.StreamSource(new java.io.File(TestCMDValidate.class.getResource("/docs/" + prof).toURI()));
+        boolean valid = cmdValidator.validateProfile(src);
+        if (valid) {
+            System.out.println("valid");
+        } else {
+            System.out.println("invalid");
+        }
+        cmdValidator.printMessages(System.out);
+        return valid;
     }
 
     @Test
     public void valid_0() throws Exception {
-	assertTrue(validate("CLARINWebService.xml"));
-	assertEquals(0, cmdValidator.getMessages().size());
+        assertTrue(validate("CLARINWebService.xml"));
+        assertEquals(0, cmdValidator.getMessages().size());
     }
 
     @Test
     public void invalid_1() throws Exception {
-	assertFalse(validate("CLARINWebService_faulty-1.xml"));
+        assertFalse(validate("CLARINWebService_faulty-1.xml"));
 
-	final List<Message> messages = cmdValidator.getMessages();
-	assertEquals(1, messages.size());
-	Message message = messages.get(0);
-	assertTrue(message.error);
-	assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/AttributeList[1]/Attribute[1]", message.location);
-	assertEquals("not(Name=('ref','ComponentId'))", message.test);
-	assertNotNull(message.text);
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals(1, messages.size());
+        Message message = messages.get(0);
+        assertTrue(message.error);
+        assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/AttributeList[1]/Attribute[1]", message.location);
+        assertEquals("not(Name=('ref','ComponentId'))", message.test);
+        assertNotNull(message.text);
     }
 
     @Test
     public void invalid_2() throws Exception {
-	assertFalse(validate("CLARINWebService_faulty-2.xml"));
+        assertFalse(validate("CLARINWebService_faulty-2.xml"));
 
-	final List<Message> messages = cmdValidator.getMessages();
-	assertEquals(1, messages.size());
-	Message message = messages.get(0);
-	assertTrue(message.error);
-	assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/AttributeList[1]/Attribute[1]/ValueScheme[1]/enumeration[1]/item[2]", message.location);
-	assertEquals("empty(preceding-sibling::item[.=current()])", message.test);
-	assertNotNull(message.text);
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals(1, messages.size());
+        Message message = messages.get(0);
+        assertTrue(message.error);
+        assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/AttributeList[1]/Attribute[1]/ValueScheme[1]/enumeration[1]/item[2]", message.location);
+        assertEquals("empty(preceding-sibling::item[.=current()])", message.test);
+        assertNotNull(message.text);
     }
 
     @Test
     public void invalid_3() throws Exception {
-	assertFalse(validate("CLARINWebService_faulty-3.xml"));
+        assertFalse(validate("CLARINWebService_faulty-3.xml"));
 
-	final List<Message> messages = cmdValidator.getMessages();
-	assertEquals(1, messages.size());
-	Message message = messages.get(0);
-	assertTrue(message.error);
-	assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/AttributeList[1]/Attribute[2]", message.location);
-	assertEquals("empty(preceding-sibling::Attribute[Name=current()/Name])", message.test);
-	assertNotNull(message.text);
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals(1, messages.size());
+        Message message = messages.get(0);
+        assertTrue(message.error);
+        assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/AttributeList[1]/Attribute[2]", message.location);
+        assertEquals("empty(preceding-sibling::Attribute[Name=current()/Name])", message.test);
+        assertNotNull(message.text);
     }
 
     @Test
     public void invalid_4() throws Exception {
-	assertFalse(validate("CLARINWebService_faulty-4.xml"));
+        assertFalse(validate("CLARINWebService_faulty-4.xml"));
 
-	final List<Message> messages = cmdValidator.getMessages();
-	assertEquals(1, messages.size());
-	Message message = messages.get(0);
-	assertTrue(message.error);
-	assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/CMD_Component[2]", message.location);
-	assertEquals("empty(preceding-sibling::*[@name=current()/@name])", message.test);
-	assertNotNull(message.text);
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals(1, messages.size());
+        Message message = messages.get(0);
+        assertTrue(message.error);
+        assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/CMD_Component[2]", message.location);
+        assertEquals("empty(preceding-sibling::*[@name=current()/@name])", message.test);
+        assertNotNull(message.text);
     }
 
     @Test
     public void invalid_5() throws Exception {
-	assertFalse(validate("CLARINWebService_faulty-5.xml"));
+        assertFalse(validate("CLARINWebService_faulty-5.xml"));
 
-	final List<Message> messages = cmdValidator.getMessages();
-	assertEquals(1, messages.size());
-	Message message = messages.get(0);
-	assertTrue(message.error);
-	assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/CMD_Element[3]", message.location);
-	assertEquals("empty(preceding-sibling::*[@name=current()/@name])", message.test);
-	assertNotNull(message.text);
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals(1, messages.size());
+        Message message = messages.get(0);
+        assertTrue(message.error);
+        assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/CMD_Element[3]", message.location);
+        assertEquals("empty(preceding-sibling::*[@name=current()/@name])", message.test);
+        assertNotNull(message.text);
     }
 
     @Test
     public void invalid_6() throws Exception {
-	assertFalse(validate("CLARINWebService_faulty-6.xml"));
+        assertFalse(validate("CLARINWebService_faulty-6.xml"));
 
-	final List<Message> messages = cmdValidator.getMessages();
-	assertEquals(1, messages.size());
-	Message message = messages.get(0);
-	assertTrue(message.error);
-	assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/CMD_Component[1]", message.location);
-	assertEquals("empty(preceding-sibling::*[@name=current()/@name])", message.test);
-	assertNotNull(message.text);
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals(1, messages.size());
+        Message message = messages.get(0);
+        assertTrue(message.error);
+        assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/CMD_Component[1]", message.location);
+        assertEquals("empty(preceding-sibling::*[@name=current()/@name])", message.test);
+        assertNotNull(message.text);
     }
 
     @Test
     public void invalid_7() throws Exception {
-	assertFalse(validate("CLARINWebService_faulty-7.xml"));
+        assertFalse(validate("CLARINWebService_faulty-7.xml"));
 
-	final List<Message> messages = cmdValidator.getMessages();
-	assertEquals(1, messages.size());
-	Message message = messages.get(0);
-	assertTrue(message.error);
-	assertNull(message.location);
-	assertNull(message.test);
-	assertNotNull(message.text);
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals(1, messages.size());
+        Message message = messages.get(0);
+        assertTrue(message.error);
+        assertNull(message.location);
+        assertNull(message.test);
+        assertNotNull(message.text);
     }
 
     @Test
     public void valid_8() throws Exception {
-	assertTrue(validate("CLARINWebService_warn-8.xml"));
+        assertTrue(validate("CLARINWebService_warn-8.xml"));
 
-	final List<Message> messages = cmdValidator.getMessages();
-	assertEquals(2, messages.size());
-	Message message = messages.get(0);
-	assertFalse(message.error);
-	assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]",message.location);
-	assertNotNull(message.test);
-	assertEquals("A nested component or element has the same name ('Service') as this component! Please, consider to rename one of them.",message.text);
-	message = messages.get(1);
-	assertFalse(message.error);
-	assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/AttributeList[1]/Attribute[1]",message.location);
-	assertNotNull(message.test);
-	assertEquals("An attribute has the same name ('Service') as its element or component! Please, consider to rename one of them.",message.text);
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals(2, messages.size());
+        Message message = messages.get(0);
+        assertFalse(message.error);
+        assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]", message.location);
+        assertNotNull(message.test);
+        assertEquals("A nested component or element has the same name ('Service') as this component! Please, consider to rename one of them.", message.text);
+        message = messages.get(1);
+        assertFalse(message.error);
+        assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/AttributeList[1]/Attribute[1]", message.location);
+        assertNotNull(message.test);
+        assertEquals("An attribute has the same name ('Service') as its element or component! Please, consider to rename one of them.", message.text);
+    }
+
+    @Test
+    public void invalid_9() throws Exception {
+        assertFalse(validate("CLARINWebService_faulty-9.xml"));
+
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals(1, messages.size());
+        Message message = messages.get(0);
+        assertTrue(message.error);
+        assertEquals("/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Component[1]/CMD_Component[2]/CMD_Component[1]/CMD_Component[2]/CMD_Component[3]", message.location);
+        assertEquals("empty(preceding-sibling::CMD_Component[@ComponentId=current()/@ComponentId])", message.test);
+        assertNotNull(message.text);
+    }
+
+    @Test
+    public void invalid_10() throws Exception {
+        assertFalse("There should be invalid cardinality values", validate("CLARINWebService_faulty-10.xml"));
+        final List<Message> messages = cmdValidator.getMessages();
+        assertEquals("There should be 5 invalid cardinality sets", 5, messages.size());
+        assertEquals("UNK, number, maximum ne 0", "/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Element[3]", messages.get(0).location);
+        assertEquals("number, UNK, minimum le 1","/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Element[6]", messages.get(1).location);
+        assertEquals("number, number, minimum le maximum","/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Element[8]", messages.get(2).location);
+        assertEquals("unbounded, UNK", "/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Element[10]", messages.get(3).location);
+        assertEquals("unbounded, 1", "/CMD_ComponentSpec[1]/CMD_Component[1]/CMD_Element[11]", messages.get(4).location);
     }
 }
