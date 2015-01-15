304254605b8105fbf163ce7f6f641d0fef382329
twagoo <t.goosen@gmail.com>
2015-01-15 15:51:41 +0000

    Added XSLT transformation logic to the ConceptRegistryServlet. Added a dummy XSLT as well to make it work without throwing exceptions.

--- ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/servlet/ConceptRegistryServlet.java
+++ ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/servlet/ConceptRegistryServlet.java
@@ -5,6 +5,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -14,6 +15,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriBuilder;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,16 +37,29 @@ import org.slf4j.LoggerFactory;
  */
 public class ConceptRegistryServlet extends HttpServlet {
 
-    private final static Logger logger = LoggerFactory.getLogger(ConceptRegistryServlet.class);
     private static final long serialVersionUID = 1L;
+    private final static Logger logger = LoggerFactory.getLogger(ConceptRegistryServlet.class);
+    private static final String CCR2DCIF_XSL_RESOURCE = "/ccr2dcif.xsl";
+
     private transient WebResource service;
+    private Templates ccr2dcifTemplates;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        URI uri = UriBuilder.fromUri(Configuration.getInstance().getCcrRestUrl()).build();
-        Client client = Client.create();
+
+        final URI uri = UriBuilder.fromUri(Configuration.getInstance().getCcrRestUrl()).build();
+        final Client client = Client.create();
         service = client.resource(uri);
+
+        try {
+            final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            final Source ccr2dcifSource = new StreamSource(getClass().getResourceAsStream(CCR2DCIF_XSL_RESOURCE));
+            ccr2dcifTemplates = transformerFactory.newTemplates(ccr2dcifSource);
+        } catch (TransformerConfigurationException ex) {
+            throw new ServletException("Could not initialise CCR to DCIF transformation templates", ex);
+        }
+
         logger.info("Instantiated CCR servlet on URI {}", uri);
     }
 
@@ -55,7 +78,7 @@ public class ConceptRegistryServlet extends HttpServlet {
         logger.info("Forwarding CCR request to {}", requestResource.getURI());
 
         // Send request to CCR
-        final String result = requestResource.accept(MediaType.APPLICATION_XML).get(String.class);
+        final InputStream result = requestResource.accept(MediaType.APPLICATION_XML).get(InputStream.class);
         logger.debug("CCR result: {}", result);
 
         final String acceptHeader = req.getHeader("Accept");
@@ -66,17 +89,28 @@ public class ConceptRegistryServlet extends HttpServlet {
         }
     }
 
-    private void serveDCIF(HttpServletResponse resp, final String ccrResult) throws IOException {
+    private void serveDCIF(HttpServletResponse resp, final InputStream ccrResponse) throws IOException, ServletException {
         // Prepare response
-        // TODO: transform to DCIF
         resp.setContentType(MediaType.APPLICATION_XML);
         resp.setCharacterEncoding("UTF-8");
 
-        resp.getWriter().write(ccrResult);
-        resp.flushBuffer();
+        try {
+            logger.debug("Transforming CCR response to response writer");
+            final Transformer transformer = ccr2dcifTemplates.newTransformer();
+            final Source source = new StreamSource(ccrResponse);
+            final Result result = new StreamResult(resp.getWriter());
+            transformer.transform(source, result);
+        } catch (TransformerConfigurationException ex) {
+            throw new ServletException("Could not configure transformer for converting CCR output to DCIF", ex);
+        } catch (TransformerException ex) {
+            throw new ServletException("Could not transform CCR output to DCIF", ex);
+        } finally {
+            ccrResponse.close();
+            resp.flushBuffer();
+        }
     }
 
-    private void serveJSON(HttpServletResponse resp, String result) throws IOException {
+    private void serveJSON(HttpServletResponse resp, final InputStream ccrResponse) throws IOException {
         //TODO: Implement JSON response
         resp.sendError(HttpServletResponse.SC_NOT_ACCEPTABLE, "JSON not supported yet - to be implemented in backend");
     }
