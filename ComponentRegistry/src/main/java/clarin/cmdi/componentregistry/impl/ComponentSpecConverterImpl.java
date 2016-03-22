0f31ed631c4f9ea12a25c013b891d1853174e5c2
Twan Goosen <twan.goosen@mpi.nl>
2016-03-22 17:03:39 +0100

    1.1 to 1.2 transformation in converter

--- ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/impl/ComponentSpecConverterImpl.java
+++ ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/impl/ComponentSpecConverterImpl.java
@@ -23,12 +23,12 @@ import org.springframework.stereotype.Service;
  */
 @Service("ComponentSpecConverter")
 public class ComponentSpecConverterImpl implements ComponentSpecConverter {
-    
+
     private final static Logger logger = LoggerFactory.getLogger(ComponentSpecConverterImpl.class);
-    
+
     private final Templates Cmdi1_1To1_2Templates;
     private final Templates Cmdi1_2To1_1Templates;
-    
+
     public ComponentSpecConverterImpl() throws TransformerConfigurationException {
         final TransformerFactory transformerFactory = TransformerFactory.newInstance();
         this.Cmdi1_1To1_2Templates = transformerFactory.newTemplates(
@@ -38,7 +38,7 @@ public class ComponentSpecConverterImpl implements ComponentSpecConverter {
                 new StreamSource(
                         CMDToolkit.class.getResourceAsStream(CMDToolkit.XSLT_COMPONENT_DOWNGRADE)));
     }
-    
+
     @Override
     public void convertComponentSpec(CmdVersion sourceVersion, CmdVersion targetVersion, InputStream specStream, Writer writer) {
         switch (sourceVersion) {
@@ -51,26 +51,31 @@ public class ComponentSpecConverterImpl implements ComponentSpecConverter {
         }
         logger.warn("Requested conversion from {} to {}, unsupported", sourceVersion, targetVersion);
     }
-    
+
     private void convert1_1Source(CmdVersion targetVersion, InputStream specStream, Writer writer) {
         if (targetVersion == CmdVersion.CMD_1_2) {
-            //TODO: upgrade 1.1 to 1.2
+            convert(Cmdi1_1To1_2Templates, specStream, writer);
         } else {
             logger.warn("Requested conversion from 1.1 to {}, unsupported", targetVersion);
         }
     }
-    
+
     private void convert1_2Source(CmdVersion targetVersion, InputStream specStream, Writer writer) {
+        if (targetVersion == CmdVersion.CMD_1_1) {
+            convert(Cmdi1_2To1_1Templates, specStream, writer);
+        } else {
+            logger.warn("Requested conversion from 1.2 to {}, unsupported", targetVersion);
+        }
+    }
+
+    private void convert(final Templates templates, InputStream specStream, Writer writer) {
         try {
-            if (targetVersion == CmdVersion.CMD_1_1) {
-                final Transformer transformer = Cmdi1_2To1_1Templates.newTransformer();
-                transformer.transform(new StreamSource(specStream), new StreamResult(writer));
-            } else {
-                logger.warn("Requested conversion from 1.2 to {}, unsupported", targetVersion);
-            }
+            final Transformer transformer = templates.newTransformer();
+            transformer.transform(new StreamSource(specStream), new StreamResult(writer));
+
         } catch (TransformerException ex) {
             logger.error("Could not carry out transformation for conversion", ex);
         }
     }
-    
+
 }
