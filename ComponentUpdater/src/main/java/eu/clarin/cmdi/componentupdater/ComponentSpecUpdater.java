3681b95ad95c5fde64899e3fcef2e1ee8140b5f7
Twan Goosen <twan.goosen@mpi.nl>
2016-05-26 16:16:40 +0200

    Set schematron phase and always fail on validation issue

--- ComponentUpdater/src/main/java/eu/clarin/cmdi/componentupdater/ComponentSpecUpdater.java
+++ ComponentUpdater/src/main/java/eu/clarin/cmdi/componentupdater/ComponentSpecUpdater.java
@@ -48,10 +48,10 @@ import org.xml.sax.SAXException;
  * @author Twan Goosen <twan@clarin.eu>
  */
 public class ComponentSpecUpdater {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(ComponentSpecUpdater.class);
     private boolean dryRun = false;
-    
+
     private ClassPathXmlApplicationContext applicationContext;
     private ComponentDao componentDao;
     private TransactionTemplate transactionTemplate;
@@ -59,7 +59,7 @@ public class ComponentSpecUpdater {
     private final Map<String, String> parameters = new HashMap<>();
     private Validator componentValidator;
     private boolean doValidate = true;
-    
+
     private void init() throws TransformerConfigurationException, SAXException {
         applicationContext = new ClassPathXmlApplicationContext("/spring-config/applicationContext.xml", "/spring-config/container-environment.xml");
         componentDao = applicationContext.getBean(ComponentDao.class);
@@ -67,8 +67,9 @@ public class ComponentSpecUpdater {
         transformer = createTransformer(parameters);
         componentValidator = new Validator(CMDToolkit.class.getResource(CMDToolkit.COMPONENT_SCHEMA));
         //TODO: pass the URL of a derived schema that adds a phase that excludes certain schematron checks we decided to skip for the upgrade
+        componentValidator.setSchematronPhase(CMDToolkit.SCHEMATRON_PHASE_CMD_COMPONENT_POST_REGISTRATION);
     }
-    
+
     private static Transformer createTransformer(final Map<String, String> transformationParams) throws IllegalArgumentException, TransformerFactoryConfigurationError, TransformerConfigurationException {
         // create transformer
         final TransformerFactory factory = TransformerFactory.newInstance();
@@ -80,13 +81,13 @@ public class ComponentSpecUpdater {
             public void warning(TransformerException exception) throws TransformerException {
                 logger.warn("Warning while transforming: {}", exception.getMessageAndLocation(), exception);
             }
-            
+
             @Override
             public void error(TransformerException exception) throws TransformerException {
                 logger.error("Error while transforming: {}", exception.getMessageAndLocation());
                 throw exception;
             }
-            
+
             @Override
             public void fatalError(TransformerException exception) throws TransformerException {
                 logger.error("Fatal arror while transforming: {}", exception.getMessageAndLocation());
@@ -96,31 +97,31 @@ public class ComponentSpecUpdater {
         applyTransformerParameters(transformer, transformationParams);
         return transformer;
     }
-    
+
     private void run() {
         if (dryRun) {
             logger.info("Dry run - no changes will be aplied to the database");
         } else {
             logger.warn("No dry run - changes will be aplied to the database!");
         }
-        
+
         if (!doValidate) {
             logger.warn("Skipping validation!");
         }
-        
+
         final List<BaseDescription> descriptions = componentDao.getAllNonDeletedDescriptions();
         logger.info("Found {} components", descriptions.size());
-        
+
         fixSpecs(descriptions);
     }
-    
+
     private void fixSpecs(final List<BaseDescription> descriptions) {
         final MutableInt success = new MutableInt(0);
         final MutableInt count = new MutableInt(0);
 
         // read and update all in a single transaction
         transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-            
+
             @Override
             protected void doInTransactionWithoutResult(TransactionStatus ts) {
                 for (final BaseDescription descr : descriptions) {
@@ -132,7 +133,7 @@ public class ComponentSpecUpdater {
                     final String status = componentDao.isPublic(id) ? "production" : "development";
                     final Map<String, String> params = new HashMap<>(1);
                     params.put("cmd-component-status", status);
-                    
+
                     final String content = componentDao.getContent(false, id);
                     if (content.contains("CMDVersion=\"1.2\"")) {
                         logger.warn("Component {} already appears to be a CMDI 1.2 component. Skipping conversion!", id);
@@ -141,11 +142,11 @@ public class ComponentSpecUpdater {
                             logger.trace("Transformation input: {}", content);
                             final String newContent = transformXml(content, params);
                             logger.debug("Transformation output: {}", newContent);
-                            
+
                             if (doValidate) {
                                 validate(id, newContent);
                             }
-                            
+
                             if (dryRun && logger.isInfoEnabled()) {
                                 final int previewLength = Math.min(500, Math.min(content.length(), newContent.length()));
                                 logger.info("Transformation result:\n-------\n{}...\n=======>\n{}...\n-------", content.substring(0, previewLength), newContent.substring(0, previewLength));
@@ -163,7 +164,7 @@ public class ComponentSpecUpdater {
         });
         logger.info("Successfully transformed {} out of {} components", success.intValue(), descriptions.size());
     }
-    
+
     private String transformXml(String content, Map<String, String> extraParams) throws TransformerException {
         final StringReader sourceReader = new StringReader(content);
         final Source source = new StreamSource(sourceReader);
@@ -171,7 +172,7 @@ public class ComponentSpecUpdater {
         final Result result = new StreamResult(resultWriter);
 
         applyTransformerParameters(transformer, extraParams);
-        
+
         try {
             transformer.transform(source, result);
             return resultWriter.toString();
@@ -181,61 +182,59 @@ public class ComponentSpecUpdater {
             applyTransformerParameters(transformer, parameters);
         }
     }
-    
+
     private void validate(final String id, final String newContent) throws RuntimeException {
         logger.debug("Validating content of {}", id);
         try {
             if (!componentValidator.validateProfile(new StreamSource(new StringReader(newContent)))) {
                 logger.error("Validation error(s) in {}: '{}'\n\n{}", id, componentValidator.getMessages(), newContent);
-                if (!dryRun) {
-                    throw new RuntimeException("Validation error");
-                }
+                throw new RuntimeException("Validation error(s) in " + id + ": " + componentValidator.getMessages().toString());
             }
         } catch (ValidatorException ex) {
             logger.error("Validation error in {}: '{}'\n\n{}", id, ex.getMessage(), newContent);
-            throw new RuntimeException("Failed to validate " + id, ex);
+            throw new RuntimeException("Error(s) while validating " + id, ex);
         } catch (IOException ex) {
             logger.error("Error reading file", ex);
-            throw new RuntimeException("Failed to validate " + id, ex);
+            throw new RuntimeException("Error(s) while validating " + id, ex);
         }
         logger.info("New content for {} is valid", id);
     }
-    
+
     public void setDryRun(boolean dryRun) {
         this.dryRun = dryRun;
     }
-    
+
     public void setDoValidate(boolean doValidate) {
         this.doValidate = doValidate;
     }
-    
+
     public Map<String, String> getParameters() {
         return parameters;
     }
-    
+
     public static void main(String[] args) throws TransformerConfigurationException, SAXException {
         final ComponentSpecUpdater updater = new ComponentSpecUpdater();
-        
+
         final List<String> arguments = Arrays.asList(args);
         updater.setDryRun(arguments.contains("-d"));
-        
+
         final String conversionParamsPropertiesFile = System.getProperty("conversionParamsPropertiesFile");
         if (conversionParamsPropertiesFile != null) {
             applyConversionParams(updater, conversionParamsPropertiesFile);
         }
-        
+
         final String skipValidation = System.getProperty("skipValidation");
         if ("true".equalsIgnoreCase(skipValidation)) {
             updater.setDoValidate(false);
         }
-        
+
         logger.info("Initializing...");
         updater.init();
-        
+
         logger.info("Running fixer...");
         updater.run();
     }
-    
+
     private static void applyConversionParams(final ComponentSpecUpdater componentSpecFixer, final String conversionParamsPropertiesFile) {
         final Map<String, String> params = componentSpecFixer.getParameters();
         try {
@@ -248,7 +247,7 @@ public class ComponentSpecUpdater {
                 logger.info("Setting stylesheet property '{}' to '{}'", keyString, value);
                 params.put(keyString, value);
             }
-            
+
         } catch (IOException ex) {
             logger.error("Could not read conversion parameters from properties file {}", conversionParamsPropertiesFile, ex);
             System.exit(1);
