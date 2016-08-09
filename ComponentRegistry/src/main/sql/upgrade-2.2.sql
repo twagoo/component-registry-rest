b8cf67e4b81126a25a2a3a344bf83cd93d9966a8
Twan Goosen <twan.goosen@mpi.nl>
2016-08-09 16:57:08 +0200

    Database upgrade script: initiate status and 'derived from' values based on XML content. Status now an enum type.

--- ComponentRegistry/src/main/sql/upgrade-2.2.sql
+++ ComponentRegistry/src/main/sql/upgrade-2.2.sql
@@ -1,4 +1,25 @@
+-- expand schema
+
+CREATE TYPE item_status as ENUM('development', 'production', 'deprecated');
+
 ALTER TABLE basedescription 
-    ADD status varchar(40), 
+    ADD status item_status, 
     ADD derivedfrom integer, 
     ADD successor integer;
+
+-- set status
+
+UPDATE basedescription SET status = 'production' WHERE is_deleted = false AND content like '%<Status>production</Status>%';
+UPDATE basedescription SET status = 'development' WHERE is_deleted = false AND content like '%<Status>development</Status>%';
+UPDATE basedescription SET status = 'deprecated' WHERE is_deleted = true OR content like '%<Status>deprecated</Status>%';
+
+-- set derived from
+
+UPDATE basedescription AS bd
+    SET derivedfrom = 
+        (SELECT id 
+            FROM basedescription 
+            WHERE component_id = regexp_replace(bd.content, '.*(<DerivedFrom>)(.*)(</DerivedFrom>).*', '\2'))
+    WHERE content LIKE '%<DerivedFrom>%';
+
+-- no need to set successor, feature not available before CompReg v2.2
