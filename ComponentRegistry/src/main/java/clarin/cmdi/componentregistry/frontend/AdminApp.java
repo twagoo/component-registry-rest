ba393f802e527981165c1f4543aa084802b0005f
Twan Goosen <twan@clarin.eu>
2020-01-24 17:10:40 +0100

    hard-configured locale

--- ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/frontend/AdminApp.java
+++ ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/frontend/AdminApp.java
@@ -1,7 +1,11 @@
 package clarin.cmdi.componentregistry.frontend;
 
+import java.util.Locale;
 import org.apache.wicket.Page;
+import org.apache.wicket.Session;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Response;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 public class AdminApp extends WebApplication {
@@ -11,8 +15,8 @@ public class AdminApp extends WebApplication {
         super.init();
         getDebugSettings().setAjaxDebugModeEnabled(false);
         getComponentInstantiationListeners().add(new SpringComponentInjector(this));
-	
-	mountPage("userSettings", UserSettingsPage.class);
+
+        mountPage("userSettings", UserSettingsPage.class);
         mountPage("accounts", Accounts.class);
         mountPage("teams", Groups.class);
         mountPage("statistics", StatisticsPage.class);
@@ -22,6 +26,13 @@ public class AdminApp extends WebApplication {
     @Override
     public Class<? extends Page> getHomePage() {
         return AdminHomePage.class;
+    }
+
+    @Override
+    public Session newSession(Request request, Response response) {
+        return super
+                .newSession(request, response)
+                .setLocale(Locale.UK);
     }
 
 }
