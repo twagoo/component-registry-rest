aaafb6747755c06ca9830908d166a3c07ffc87e0
Twan Goosen <twan@clarin.eu>
2020-11-24 17:16:20 +0100

    Added updated of component definition in database to 2.4 db upgrade script. Refs #58

--- ComponentRegistry/src/main/sql/upgrade-2.4.sql
+++ ComponentRegistry/src/main/sql/upgrade-2.4.sql
@@ -4,3 +4,20 @@ ALTER TABLE basedescription
     ADD recommended boolean DEFAULT 'false' NOT NULL;
 
 CREATE INDEX idx_basedescription_recommended ON basedescription USING btree (recommended);
+
+BEGIN TRANSACTION;
+
+UPDATE basedescription 
+    SET content = replace(content, 'http://www.clarin.eu/cmdi/cues/1', 'http://www.clarin.eu/cmd/cues/1')
+    WHERE content LIKE '%http://www.clarin.eu/cmdi/cues/1%';
+
+SELECT
+    CASE 
+        WHEN 0 = count(*) 
+        THEN 'Successfully updated cue namespace in component definitions' 
+        ELSE 'ERROR while updating cue namespace in component definitions' 
+    END as "Update result"
+    FROM basedescription 
+        WHERE content LIKE '%http://www.clarin.eu/cmdi/cues/1%';
+
+COMMIT;
